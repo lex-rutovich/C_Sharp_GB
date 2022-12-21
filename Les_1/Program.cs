6d8cfea73e6394cdb3e78b4b06fb9fdfe43e6eae
lex-rutovich <alexbut_kz@bk.rudotnet>
2022-12-22 00:12:17 +0300

    Lesson1

--- Les_1/Program.cs
+++ Les_1/Program.cs
@@ -12,4 +12,58 @@ Console.WriteLine("2 Hello, World!");
 // Console.Write("Привет, ");
 // Console.Write(username);
 
+// Написать программу на языке C# для Windows и Mac OS,
+// [в которую пользователь вводит два числа]
+// [в которой записаны два числа]
+// [два числа генерируются случайным образом]
+// и получает сумму
+
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+// или:
+
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// int numberA = 12;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// int numberA = new Random().Next(1, 10); //1 2 3 4 ... 9
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// Начало
+// Ввести имя пользователя
+// ЕСЛИ имя пользователя совпадает с "Маша" то...
+// поприветствовать по особенному
+// ИНАЧЕ поприветствовать по обычному
+// Конец
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+// 
+// if(username.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username);
+// }
+
+
+
 
