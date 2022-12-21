ecf0acb459fd46a48d37318b42efdf867ffe2926
lex-rutovich <alexbut_kz@bk.rudotnet>
2022-12-22 01:41:12 +0300

    End

--- Les_1/Program.cs
+++ Les_1/Program.cs
@@ -64,6 +64,87 @@ Console.WriteLine("2 Hello, World!");
 //     Console.WriteLine(username);
 // }
 
+// Example006:
+// 
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e =4;
+// 
+// int max = a;
+// 
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+// 
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+/*
+Синтаксис:начало
+
+Алгоритм:
+1.Определить три точки
+2.Выбрать две любых
+3.Найти середину
+4.Поставить точку
+5.Выбрать случайную вершину треугольника
+6.Соединить её с полученной на 4 шаге точкой
+7.Перейти к шагу 3
+8.Шаги 3 - 7 повторить 9,28,31 раз
+clear
+Console.Clear();
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+*/
+
+int xa = 30, ya = 1, 
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+    
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10000);
+{
+    int what = new Random().Next(0, 3);   // [0;3) -полуинтервал, могут выдоваться числа 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1; // Можно использовать такую запись: count ++;
+}
+
+// фрактальное изображение Треугольник Серпинского
 
 
 
