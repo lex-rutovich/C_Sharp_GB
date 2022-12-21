bf0dd8f659ea3d0d42f1cc60d978263e67bfeb13
lex-rutovich <alexbut_kz@bk.rudotnet>
2022-12-21 21:01:58 +0300

    Hello_Console

--- Les_1/Program.cs
+++ Les_1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* See https://aka.ms/new-console-template for more information
+*/
+
+Console.WriteLine("2 Hello, World!");
+
