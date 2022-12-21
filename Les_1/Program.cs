1d33189bf6ae1d371aa32549699a8db4c6b7c0d9
lex-rutovich <alexbut_kz@bk.rudotnet>
2022-12-21 21:21:55 +0300

    Cons

--- Les_1/Program.cs
+++ Les_1/Program.cs
@@ -1,5 +1,15 @@
 ﻿/* See https://aka.ms/new-console-template for more information
+Console.WriteLine("2 Hello, World!");
 */
 
-Console.WriteLine("2 Hello, World!");
+// Console.WriteLine("Введите ваше имя ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+// Console.Write("Введите ваше имя ");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
+
 
